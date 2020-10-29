5738f0df272530514a47742f4b64acaef354d043
A.Sikorski <sikorski.aps@gmail.com>
2020-10-29 21:10:39 +0100

    Minor fixes

--- app/src/main/java/com/example/covidmind/ui/dashboard/ActivitiesFragment.kt
+++ app/src/main/java/com/example/covidmind/ui/dashboard/ActivitiesFragment.kt
@@ -10,7 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.covidmind.R
 
-class DashboardFragment : Fragment() {
+class ActivitiesFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
@@ -21,7 +21,7 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val root = inflater.inflate(R.layout.fragment_activities, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(this, Observer {
             textView.text = it
--- app/src/main/java/com/example/covidmind/ui/notifications/SettingsFragment.kt
+++ app/src/main/java/com/example/covidmind/ui/notifications/SettingsFragment.kt
@@ -10,7 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.covidmind.R
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
 
@@ -21,7 +21,7 @@ class NotificationsFragment : Fragment() {
     ): View? {
         notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(this, Observer {
             textView.text = it
