f7d0965c8811d38ab3a2b3f95aaa5a4c253552b3
A.Sikorski <sikorski.aps@gmail.com>
2020-12-30 18:43:53 +0100

    Minor visual changes

--- app/src/main/java/com/example/covidmind/ui/dashboard/ActivitiesFragment.kt
+++ app/src/main/java/com/example/covidmind/ui/dashboard/ActivitiesFragment.kt
@@ -22,10 +22,6 @@ class ActivitiesFragment : Fragment() {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_activities, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
